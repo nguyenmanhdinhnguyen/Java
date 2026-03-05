32495780f473ef77a902cdd38eaaff17ef8676fb
nguyenmanhdinhnguyen <nguyen.nmd.66cntt@ntu.edu.vn>
2026-03-05 11:55:32 +0700

    update helloworld.java

---
+++ BT1_HelloWorld/helloworld.java
@@ -0,0 +1,13 @@
+package Java.BT1_HelloWorld;
+
+public class helloworld{
+    public static void main(String[] args){
+        String greeting = """
+            ============================
+            HELLO, JAVA!
+            ============================
+            by Nguyen Manh Dinh Nguyen
+                """;
+        System.out.println(greeting);
+    }
+}
